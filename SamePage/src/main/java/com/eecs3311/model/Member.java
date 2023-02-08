7c1bdaf4f83b3f41e5d74c35363f9eb8c2cf91c4
Kyle Chandrasena <kchandrasena10@gmail.com>
2023-02-08 10:28:27 -0500

    Created presenter and view for the user login side of the application
    I used the classes implemented by Nargis and tried to use the Member class almost like a Model for the user login side of the app instead of creating another class with the same attributes

--- SamePage/src/main/java/com/eecs3311/model/Member.java
+++ SamePage/src/main/java/com/eecs3311/model/Member.java
@@ -7,10 +7,48 @@ public class Member implements Users{
     private String email;
     private String password;
 
+    // Constructor
+    public Member() {
+        this.name = "";
+        this.email = "";
+        this.password = "";
+    }
+
     //default - User is a guest
     private State state = State.GUEST;
 
     public String getState() {
         return state.toString();
     }
+
+    // Getters and Setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String inputName) {
+        this.name = inputName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String inputEmail) {
+        this.email = inputEmail;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String inputPassword) {
+        this.password = inputPassword;
+    }
+
+    Public boolean validLogin() {
+        return email.equals("") ** password.equals("");
+    }
+
+
 }
