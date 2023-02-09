d2fd6c8fadcc85d464808857c60a4459f5b3fc35
Kyle Chandrasena <kchandrasena10@gmail.com>
2023-02-08 23:35:19 -0500

    Created two versions for LoginView UI for testing

--- SamePage/src/main/java/com/eecs3311/view/layout/LoginView.java
+++ SamePage/src/main/java/com/eecs3311/view/layout/LoginView.java
@@ -24,45 +24,119 @@ public class LoginView extends JFrame implements ILoginView {
 
     public LoginView() {
     	
+    	// Lines 28 - 108 is a organized version of the Login Page however the login functionality does not work, on click of button the login status does not change
+    	
     	JTextField emailField = new JTextField();
         JPasswordField passwordField = new JPasswordField();
         JLabel email = new JLabel("Email:\n");
         JLabel password = new JLabel("Password\n");
-        JLabel login = new JLabel("Login to SamePage");
+    	JLabel lblHeader = new JLabel("Login to SamePage");
         JButton loginSubmit = new JButton("Login");
-        JLabel loginStatus = new JLabel("");
+        JLabel loginStatus = new JLabel("Status");
         
         email.setFont(mainFont);
         password.setFont(mainFont);
         login.setFont(mainFont);
         loginStatus.setFont(mainFont);
         
-        Border border = BorderFactory.createLineBorder(Color.BLACK, 5);
+        
+        Border border = BorderFactory.createLineBorder(Color.BLACK, 1);
         emailField.setBorder(border);
         passwordField.setBorder(border);
         
     	
     	
-        JPanel formPanel = new JPanel();
-        
-        formPanel.add(login);
-        login.setHorizontalAlignment(JLabel.CENTER);
-
-        JPanel mainPanel = new JPanel();
-        mainPanel.setLayout(new BorderLayout());
-        mainPanel.setBackground(new Color(128, 128, 255));
-        mainPanel.add(formPanel, BorderLayout.NORTH);
-//        mainPanel.add(email, BorderLayout.CENTER);
-//        mainPanel.add(emailField, BorderLayout.CENTER);
-//        mainPanel.add(password, BorderLayout.CENTER);
-//        mainPanel.add(passwordField, BorderLayout.CENTER);
+//        JPanel formPanel = new JPanel();
+//        
+//        formPanel.add(login);
+//        login.setHorizontalAlignment(JLabel.CENTER);
 //
-//        mainPanel.add(loginSubmit, BorderLayout.SOUTH);
-//        mainPanel.add(loginStatus, BorderLayout.SOUTH);
-
-
-        add(mainPanel);
+//        JPanel mainPanel = new JPanel();
+//        mainPanel.setLayout(new BorderLayout());
+//        mainPanel.setBackground(new Color(128, 128, 255));
+//        mainPanel.add(formPanel, BorderLayout.NORTH);
+////        mainPanel.add(email, BorderLayout.CENTER);
+////        mainPanel.add(emailField, BorderLayout.CENTER);
+////        mainPanel.add(password, BorderLayout.CENTER);
+////        mainPanel.add(passwordField, BorderLayout.CENTER);
+////
+////        mainPanel.add(loginSubmit, BorderLayout.SOUTH);
+////        mainPanel.add(loginStatus, BorderLayout.SOUTH);
+//
+//
+//        add(mainPanel);
         
+        JPanel panel = new JPanel(new GridBagLayout());
+        GridBagConstraints constraints = new GridBagConstraints();
+        constraints.insets = new Insets(5, 5, 5, 5);
+        
+        constraints.gridx = 1;
+        constraints.gridy = -2;
+        panel.add(lblHeader, constraints);
+	    lblHeader.setHorizontalAlignment(JLabel.CENTER);
+
+
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+        panel.add(new JLabel("Email:"), constraints);
+
+        constraints.gridx = 1;
+        panel.add(emailField, constraints);
+
+        constraints.gridx = 0;
+        constraints.gridy = 1;
+        panel.add(new JLabel("Password:"), constraints);
+
+        constraints.gridx = 1;
+        panel.add(passwordField, constraints);
+
+        constraints.gridx = 0;
+        constraints.gridy = 2;
+        constraints.gridwidth = 2;
+        constraints.anchor = GridBagConstraints.WEST;
+        panel.add(loginSubmit, constraints);
+
+        constraints.gridx = 0;
+        constraints.gridy = 3;
+        constraints.gridwidth = 2;
+        constraints.anchor = GridBagConstraints.WEST;
+        panel.add(loginStatus, constraints);
+        
+        emailField.setPreferredSize(new Dimension(200, emailField.getPreferredSize().height));
+        passwordField.setPreferredSize(new Dimension(200, passwordField.getPreferredSize().height));
+        
+        add(panel);
+        
+        // Lines 110 - 136 is the implementation for a working version of the Login Page however the layout is unorganized
+	
+//	private JTextField emailField = new JTextField(10);
+//	  private JPasswordField passwordField = new JPasswordField(10);
+//	  private JButton loginSubmit = new JButton("Login");
+//	  private JLabel lblHeader = new JLabel("Login to SamePage");
+//	  private JLabel loginStatus = new JLabel("status pending");
+//
+//	  public LoginView() {
+//	    setTitle("Login");
+//	    setSize(300, 150);
+//	    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//	    setLocationRelativeTo(null);
+//
+//	    JPanel panel = new JPanel();
+//	    panel.setLayout(new GridLayout(3, 2, 10, 10));
+//	    panel.add(new JLabel("Email:"));
+//	    panel.add(emailField);
+//	    panel.add(new JLabel("Password:"));
+//	    panel.add(passwordField);
+//	    panel.add(new JPanel());
+//	    panel.add(loginSubmit);
+//	    panel.add(loginStatus);
+//
+//	    add(lblHeader, BorderLayout.NORTH);
+//	    add(panel, BorderLayout.CENTER);
+//
+//	    lblHeader.setHorizontalAlignment(JLabel.CENTER);
+//	    loginStatus.setHorizontalAlignment(JLabel.CENTER);
+////        
 
         setTitle("Login Page");
         setSize(500, 600);
@@ -92,6 +166,7 @@ public class LoginView extends JFrame implements ILoginView {
         Member model = new Member();
         LoginView view = new LoginView();
         LoginPresenter presenter = new LoginPresenter(view, model);
+        System.out.println("Login window opened");
         
         view.setVisible(true);
       }
