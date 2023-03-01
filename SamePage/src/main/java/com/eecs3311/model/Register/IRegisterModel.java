ef64dfd62d3dda858c74910ca2f4acd3f512a8f0
Kyle Chandrasena <kchandrasena10@gmail.com>
2023-03-01 16:42:55 -0500

    RegisterModel and interface implementation

--- SamePage/src/main/java/com/eecs3311/model/Register/IRegisterModel.java
+++ SamePage/src/main/java/com/eecs3311/model/Register/IRegisterModel.java
@@ -1,5 +1,45 @@
 package com.eecs3311.model.Register;
 
+import com.eecs3311.presenter.Register.IRegisterPresenter;
+import com.eecs3311.persistence.Register.RegisterDB;
+
+
 public interface IRegisterModel {
+/**
+     * Return the presenter
+     * 
+     * @return IRegisterPresenter
+     */
+    IRegisterPresenter getPresenter();
+
+    /**
+     * Sets the presenter
+     * 
+     * @param ilp
+     */
+    void setPresenter(IRegisterPresenter ilp);
+
+    /**
+     * Updates the model from interactions from the view
+     * 
+     * @param username
+     * @param email
+     * @param password
+     */
+    void updateModelFromView(String username, String email, String password);
+    
+    /**
+     * Return the Register persistence class (connection to same page user database)
+     * @return
+     */
+    RegisterDB getRegisterDB();
+
+    /**
+     * Sets the Register persistence class (connection to same page user database)
+     * @param rdb
+     */
+    void setRegisterDB(RegisterDB rdb);
+
 
 }
+
--- SamePage/src/main/java/com/eecs3311/model/Register/RegisterModel.java
+++ SamePage/src/main/java/com/eecs3311/model/Register/RegisterModel.java
@@ -1,8 +1,71 @@
 package com.eecs3311.model.Register;
 
 import com.eecs3311.presenter.Register.IRegisterPresenter;
+import com.eecs3311.persistence.Register.RegisterDB;
+
 
 public class RegisterModel implements IRegisterModel {
+    private IRegisterPresenter RegisterPresenter;
+    private RegisterDB registerDB;
+    private String username;
+    private String email;
+    private String password;
 
-    
+    @Override
+    public IRegisterPresenter getPresenter() {
+        return RegisterPresenter;
+    }
+
+    @Override
+    public void setPresenter(IRegisterPresenter ilp) {
+        this.RegisterPresenter = ilp;
+    }
+
+    @Override
+    public void updateModelFromView(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+
+        validateRegister();
+    }
+
+    private void validateRegister() {
+        // Write Query
+        // Send query to db
+        // Get results from db
+        // Compare results
+        // Based on results, update view
+
+        String queryResult = registerDB.registerUser(this.username, this.email, this.password);
+
+        this.getPresenter().updateViewFromModel(queryResult);
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+	public RegisterDB getRegisterDB() {
+		return this.registerDB;
+	}
+
+	@Override
+	public void setRegisterDB(RegisterDB rdb) {
+		this.registerDB = rdb;
+	}
+
 }
