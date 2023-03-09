229ba7ec57edcacbb992d34009efd11fcdf5cbaf
nargisgh <nargisghiasi@gmail.com>
2023-03-09 18:59:31 -0500

    Update meeting log

---
+++ SamePage/src/test/java/com/eecs3311/integrationtests/BookIntegrationTest.java
@@ -0,0 +1,16 @@
+package com.eecs3311.integrationtests;
+
+import org.junit.jupiter.api.Test;
+
+public class BookIntegrationTest {
+    @Test
+    void getUsername() {
+    }
+    //user searches book, compare
+
+    //user views landing, compare
+
+    //user adds reviews to book, compare
+
+    //user adds book to favorites, compare
+}
