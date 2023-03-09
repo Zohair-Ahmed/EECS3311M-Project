d80ac52c6aa09503bcc94a18c09579ebb1402ff1
nargisgh <nargisghiasi@gmail.com>
2023-03-09 13:49:43 -0500

    Update User, Register and Login Stubs for Unit testing

--- SamePage/src/main/java/com/eecs3311/persistence/Login/LoginStub.java
+++ SamePage/src/main/java/com/eecs3311/persistence/Login/LoginStub.java
@@ -1,9 +1,38 @@
 package com.eecs3311.persistence.Login;
 
+import com.eecs3311.model.User;
+import com.eecs3311.model.UserStub;
+
+import java.util.ArrayList;
+
 public class LoginStub implements ILogin{
+
+    private ArrayList<UserStub> users;
+    UserStub userStub = UserStub.getInstance();
+    private static LoginStub instance = null;
+
+    private LoginStub(){
+        users = userStub.userList();
+    }
+    public static LoginStub getInstance(){
+        if(instance == null){
+            instance = new LoginStub();
+        }
+        return instance;
+    }
 
     @Override
     public boolean isLoginValid(String email, String password) {
+        int i =0;
+        while(i < users.size()){
+            if(users.get(i).getEmail().equals(email) && users.get(i).getPassword().equals(password)){
+                User.getInstance().setEmail(email);
+                User.getInstance().setUsername(users.get(i).getUsername());
+                User.getInstance().setPassword(password);
+                return true;
+            }
+            i++;
+        }
         System.out.println("This is the Login Stub DB");
         return false;
     }
