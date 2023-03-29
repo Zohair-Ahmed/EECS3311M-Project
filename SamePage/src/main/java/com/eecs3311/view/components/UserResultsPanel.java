c8c1389943c8ef7681ca667dfa3e98f9aafc405c
nargisgh <56950238+nargisgh@users.noreply.github.com>
2023-03-29 00:24:53 -0400

    Feature/search friends (#95)
    * Refactor image layout for username view
    
    * Refactored UI for user search
    
    * Left anchor for username
    
    * increased font size for landing page title
    
    ---------
    
    Co-authored-by: Nicholas Lachhman <nickla@my.yorku.ca>

--- SamePage/src/main/java/com/eecs3311/view/components/UserResultsPanel.java
+++ SamePage/src/main/java/com/eecs3311/view/components/UserResultsPanel.java
@@ -2,6 +2,7 @@ package com.eecs3311.view.components;
 
 import com.eecs3311.persistence.Database;
 import com.eecs3311.view.IPanelView;
+import com.eecs3311.view.User.UserView;
 
 import javax.swing.*;
 import java.awt.*;
@@ -57,7 +58,7 @@ public class UserResultsPanel implements ActionListener, IPanelView {
         GridBagConstraints c = new GridBagConstraints();
         c.fill = GridBagConstraints.HORIZONTAL;
         c.ipadx = 1250;
-        c.ipady = 300;
+        c.ipady = 425;
         c.weightx = 0.0;
         c.gridwidth = 1;
         c.gridx = 0;
@@ -68,7 +69,7 @@ public class UserResultsPanel implements ActionListener, IPanelView {
             this.textJLabel.setText(results.size() + " " + ((results.size() == 1 ? "result" : "results") + " found..."));
             scroll = new JScrollPane(releaseContainer, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
                     JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
-            releaseContainer.setLayout(new GridLayout((int)Math.ceil(results.size()/7)+1, 7));
+            releaseContainer.setLayout(new GridLayout((int)Math.ceil(results.size()/4)+1, 4));
         }
         container.add(scroll, c);
     }
@@ -76,32 +77,19 @@ public class UserResultsPanel implements ActionListener, IPanelView {
     private void initReleaseContainer(ArrayList<String> results) {
         if (results == null)
             return;
-        GridLayout gridLayout = new GridLayout((int)Math.ceil(results.size()/7)+1, 7);
+        GridLayout gridLayout = new GridLayout((int)Math.ceil(results.size()/4)+1, 4);
         GridBagConstraints c = new GridBagConstraints();
-        ImageIcon imageIcon = new ImageIcon(new ImageIcon(this.getClass().getResource("/images/profileimg.png")).getImage().getScaledInstance(80, 80, Image.SCALE_SMOOTH));
-        JLabel picLabel = new JLabel(imageIcon);
+        c.gridheight = 100;
+        c.gridwidth = 100;
         releaseContainer.setLayout(gridLayout);
-        releaseContainer.add(picLabel, c);
         results.parallelStream().forEach(ibm -> {
-            JLabel label = new JLabel(ibm);
-            label.addMouseListener(onUserClicked(label.getText()));
-            releaseContainer.add(label);
+            UserView userView = new UserView(ibm);
+            releaseContainer.add(userView.getView(),c);
         });
         initTextLayout();
         initScrollPaneView(results);
     }
 
-    private MouseListener onUserClicked(String label) {
-        return new MouseAdapter()
-        {
-            @Override
-            public void mousePressed(MouseEvent e)
-            {
-                System.out.printf("Clicked %s\n",label);
-                JOptionPane.showMessageDialog(releaseContainer, "Selected user "+label);
-            }
-        };
-    }
 
     @Override
     public void actionPerformed(ActionEvent e) {
