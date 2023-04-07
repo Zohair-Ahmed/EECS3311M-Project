5b0fa947d2ad9316c365607e25c84f7609209b31
nargisgh <nargisghiasi@gmail.com>
2023-04-07 01:40:21 -0400

    JUNIT testing for followers

--- SamePage/src/main/java/com/eecs3311/persistence/Follower/FollowerStub.java
+++ SamePage/src/main/java/com/eecs3311/persistence/Follower/FollowerStub.java
@@ -1,10 +1,28 @@
 package com.eecs3311.persistence.Follower;
 
-import com.eecs3311.model.Review.IReviewModel;
-
+import com.eecs3311.model.Follower.IFollowerModel;
 import java.util.ArrayList;
 
 public class FollowerStub {
 
-    private final static ArrayList<IReviewModel> reviewList = new ArrayList<>();
+    private final static ArrayList<IFollowerModel> followerList = new ArrayList<>();
+
+    private static FollowerStub instance = null;
+    public void addFollower(IFollowerModel followedUser) {
+        followerList.add(followedUser);
+    }
+
+    public ArrayList<IFollowerModel> getFollowerList() {
+        return followerList;
+    }
+    public static FollowerStub getInstance(){
+        if(instance == null){
+            instance = new FollowerStub();
+        }
+        return instance;
+    }
+
+    public void removeFollower(IFollowerModel followedUser) {
+        followerList.remove(followedUser);
+    }
 }
